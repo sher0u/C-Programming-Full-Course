78954cb345375d9d3cd6f5fd38c1211374bcef45
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2023-11-21 00:51:15 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//This is comment you can type what ever you want and it will not be ignored by the compiler but this is just for the one line
+/*This is the second Type of comment you can use it for
+ * many lines */
+int main() {
+    printf("Welcome To My Cours of The Programming Languge C!\n");// the symbol of /n mean a new line
+    printf("By Yousfi Abdelkader");
+    return 0; //this return 0 if the program work normal if no will return 1
+}
