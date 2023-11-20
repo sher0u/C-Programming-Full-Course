868d8db7c9881b4d3a0c3bdcf8a8d70027843827
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2023-11-21 00:42:00 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Welcome To My Cours of The Programming Languge C!\n");// the symbol of /n mean a new line
+    printf("By Yousfi Abdelkader");
+    return 0; //this return 0 if the program work normal if no will return 1
+}
