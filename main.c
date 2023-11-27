e3e446c2193d6c832977c90a1c15bea50368ea5a
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2023-11-28 01:58:18 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Arththemetic Operation
+/*
+ * + addition.
+ * - substraction .
+ * * multipilication.
+ * / division.
+ * % module mean the rest of the division.
+ * ++ increament.
+ * -- decrement .
+ */
+int main(){
+
+    int a=2,b=3;
+    printf("%d\n", a+b);
+    printf("%d", a++);
+    return 0;
+}
