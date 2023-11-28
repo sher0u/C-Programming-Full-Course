4215a6752c1e722bff71f409b4750ba615d975d9
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2023-11-28 23:31:13 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(){
+
+        int age;
+        char name[25];
+    printf("How old are you ??");
+    scanf("%d",&age);
+    printf("What is your name ??");
+    scanf("%s",name);// but this will just get the charchter till the space.
+    printf("My age is = %d ,and my name is %s...:)\n",age,name);
+    fgets(name , 25 , stdin);// to read the space line .
+    printf("Your full name is %s",name);
+    return 0;
+}
