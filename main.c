b41c82bc34a62fc231f2edaab152b7a5ccdf7722
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2023-11-30 00:38:45 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// If Stattment (Condition).
+int main(){
+    int age;
+
+    printf("How old are you Danil");
+    scanf("%d",&age);
+
+    if (age >= 18) // we can use in the condition > or < or <= or >= or == or !=
+    {
+        printf("You can buy A cigarete With you passport");
+    }
+    else
+    {
+        printf("Call abdelakder As always ");
+    }
+    // Observation you can use if or you can use if else and you can use if else if .
+    // and danil is my freind he have 18 right now say happy birthday to him .:)
+    return 0;
+}
